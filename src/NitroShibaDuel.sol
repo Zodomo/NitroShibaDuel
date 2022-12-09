6053cd8c9f764f5adf8bd315f480654ea9240538
Zodomo <zodomo@proton.me>
2022-12-09 15:59:04 -0600

    further reworked data structures

--- src/NitroShibaDuel.sol
+++ src/NitroShibaDuel.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.17;
 import "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-contracts/token/ERC721/IERC721Receiver.sol";
 import "openzeppelin-contracts/utils/Address.sol";
+import "openzeppelin-contracts/utils/Counters.sol";
 
 contract NitroShibaDuel {
 
     using Address for address;
+    using Counters for Counters.Counter;
 
     /*//////////////////////////////////////////////////////////////
                 STORAGE
@@ -18,6 +20,9 @@ contract NitroShibaDuel {
     address immutable nishibToken = 0x4DAD357726b41bb8932764340ee9108cC5AD33a0;
     address immutable nishibNFT = 0x74B8e48823658af4296814a8eC6baf271BcFa1e0;
 
+    // Incremential duel count value is used as duel identifier
+    Counters.Counter public duelCount;
+
     // Stores user $NISHIB balances for contract logic
     mapping(address => uint256) public nishibBalances;
 
@@ -26,7 +31,8 @@ contract NitroShibaDuel {
         SimpleBet,
         DoubleOrNothing,
         PVP,
-        PVPPlus
+        PVPPlus,
+        Jackpot
     }
     // Status enum determines duel status
     enum Status {
@@ -37,17 +43,18 @@ contract NitroShibaDuel {
 
     // Duel struct handles interaction and match data
     struct Duel {
-        address initiator; // Address of whoever initiates Duel
-        uint256 initiatorNFT; // tokenID of the NFT initiator is dueling with
-        address challenger; // Address of whoever challenges the initiator
-        uint256 challengerNFT; // Challenger's dueling NFT tokenID
-        uint256 bet; // $NISHIB bet
+        address[] addresses; // Player addresses, first is initiator
+        uint256[] tokenIDs; // Player NFT tokenIDs, one per player, indexed by player address
+        uint256[] outcomes; // Player VRF outcome values, highest always wins
+        uint256 bet; // Static $NISHIB bet per player
         Mode mode; // Game mode
         Status status; // Game status
-        uint256 outcome; // VRF value determining winner <0.5 for initiator, >0.5 for challenger, rerolled if tie
+        address winner; // Winner address
+        uint256 tokenPayout; // Total $NISHIB payout
+        uint256 nftPayout; // NFT payout, if any
     }
     // bytes32 duel identifier to Duel struct
-    mapping(bytes32 => Duel) public duels;
+    mapping(uint256 => Duel) public duels;
 
     /*//////////////////////////////////////////////////////////////
                 CONSTRUCTOR
